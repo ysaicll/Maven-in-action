52266ec769ec32336b5418f90903d07273a6773c
Leo Sheng <ysaicll@sina.com>
2017-09-26 14:24:57 +0800

    hello world program

---
+++ helloworld/src/main/java/com/ruc/mvn/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.ruc.mvn.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
